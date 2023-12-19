87bf3717c537d96c3cb017eaa79bfa8a4490dd8c
Yash Pandey <pandeyyash613@gmail.com>
2023-12-19 23:49:18 +0530

    Added user controller, completed Frontend

--- src/components/AdmissionForm.js
+++ src/components/AdmissionForm.js
@@ -1,12 +1,14 @@
-// src/components/AdmissionForm.js
-
 import React, { useState } from "react";
+import "./AdmissionForm.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AdmissionForm = () => {
   const [formData, setFormData] = useState({
     name: "",
-    age: "",
-    selectedBatch: "",
+    email: "",
+    age: 0,
+    batch: "",
   });
 
   const handleInputChange = (e) => {
@@ -15,45 +17,88 @@ const AdmissionForm = () => {
       [e.target.name]: e.target.value,
     });
   };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // TODO: Make API call to backend with formData
+
+    try {
+      const response = await fetch("http://localhost:5000/api/enroll", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        toast.success("Enrolled successfully", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        // Additional logic if needed
+      } else {
+        toast.error(
+          "Failed to submit form to change the slot move to next month",
+          {
+            position: toast.POSITION.TOP_CENTER,
+          }
+        );
+        // Handle error
+      }
+    } catch (error) {
+      toast.error(`Error submitting form: ${error}`, {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      // Handle error
+    }
   };
 
+  // ... (remaining code)
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Name:</label>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleInputChange}
-      />
-
-      <label>Age:</label>
-      <input
-        type="number"
-        name="age"
-        value={formData.age}
-        onChange={handleInputChange}
-      />
-
-      <label>Select Batch:</label>
-      <select
-        name="selectedBatch"
-        value={formData.selectedBatch}
-        onChange={handleInputChange}
-      >
-        <option value="">Select Batch</option>
-        <option value="6-7AM">6-7AM</option>
-        <option value="7-8AM">7-8AM</option>
-        <option value="8-9AM">8-9AM</option>
-        <option value="5-6PM">5-6PM</option>
-      </select>
-
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <ToastContainer />
+      <form className="AdmissionForm" onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+        <br></br>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+        <br></br>
+        <label>Age:</label>
+        <input
+          type="number"
+          name="age"
+          value={formData.age}
+          onChange={handleInputChange}
+        />
+        <br></br>
+        <label>Select Batch:</label>
+        <select
+          name="batch"
+          value={formData.batch}
+          onChange={handleInputChange}
+        >
+          <option value="">Select Batch</option>
+          <option value="6-7AM">6-7AM</option>
+          <option value="7-8AM">7-8AM</option>
+          <option value="8-9AM">8-9AM</option>
+          <option value="5-6PM">5-6PM</option>
+        </select>
+        <br></br>
+        <button className="submit" type="submit">
+          Enroll
+        </button>
+      </form>
+    </>
   );
 };
 
